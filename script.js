1cb6525e149be4a06f5604f89dbe04bee1242e3d
Sif Melkane <siha0004@stud.kea.dk>
2024-01-31 22:12:59 +0100

    Opgave 1 løst

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hejsa");
+window.addEventListener("load", sidenVises);
+
+let promille;
+
+function sidenVises() {
+  console.log("Siden Vises");
+  document.querySelector("#cola").addEventListener("mousedown", alkoholfri);
+  document.querySelector("#faxe").addEventListener("mousedown", alkoholfri);
+  document.querySelector("#beer").addEventListener("mousedown", alkohol);
+  document.querySelector("#snaps").addEventListener("mousedown", alkohol);
+}
+
+function alkohol() {
+  promille = 1;
+
+  alkoholTjek();
+}
+
+function alkoholfri() {
+  promille = 0;
+
+  alkoholTjek();
+}
+
+function alkoholTjek() {
+  console.log("alkoholTjek");
+
+  if (promille == 0) {
+    console.log("Drikken er alkoholfri");
+  } else {
+    console.log("Drikken indeholder alkohol");
+  }
+}
